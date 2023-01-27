c21353a972618ac3ea7ee83788f431f4f4cf2ab1
Ridwan Efendi <ridwanefendi1903@gmail.com>
2023-01-27 16:22:32 +0700

    first commit

---
+++ assets/BaseButton.be66e1ab.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,c as s,t as o}from"./index.fb6dfa4b.js";const a={name:"BaseButton",props:{name:String}},_={class:"btn"};function c(r,i,t,p,B,u){return n(),s("div",_,o(t.name),1)}const m=e(a,[["render",c]]);export{m as B};
